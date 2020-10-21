3487cefb2d030885eabd318e046ed20b1a9e056e
amang <amang55comsci@gmail.com>
2020-10-21 14:57:28 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'fcNEWnXyG8hljDKRaVBF9pmgEHW90iq+gqhRi6cNdV1vP2943jDevvRgr+tZAspqzOCZpKv9/V/erpxR11JOWMxcKN3X2ixhO0yM+kExpzfj6yfvJG37BbF84fMO9KaLPnNBPm38R1G/usb0DlIfhAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'yteuBCkFEutiGBOT+Ddx3PByNMBEkb3NOVdwnitzhOY+BdWynS6Z0ANgwkFVCiJFzOCZpKv9/V/erpxR11JOWMxcKN3X2ixhO0yM+kExpzdxLomNB9lxQQXQRd8tmZnoMKoxB3icD0abqqDUrF9PlwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
