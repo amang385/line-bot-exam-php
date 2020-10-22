853d316f9a52088f118a5b9d7fc64e1c9d735ed4
amang <amang55comsci@gmail.com>
2020-10-23 00:34:39 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." ---groupId--".$event['source']['groupId']."----".$event['source']
+				'text' => $text." ---groupId--".$event['source']['groupId']."--roomId--".$event['source']['roomId']
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
